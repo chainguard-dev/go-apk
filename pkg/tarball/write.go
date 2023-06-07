2127ae6b7dc8220606ddb1fe709c258e7d928522
Josh Wolf <josh@wolfs.io>
2023-06-07 09:36:57 -0400

    add WithBaseDir option to tar context
    Signed-off-by: Josh Wolf <josh@wolfs.io>

--- pkg/tarball/write.go
+++ pkg/tarball/write.go
@@ -23,6 +23,7 @@ import (
 	"io"
 	"io/fs"
 	"os"
+	"path/filepath"
 	"syscall"
 
 	gzip "golang.org/x/build/pargzip"
@@ -143,7 +144,7 @@ func (c *Context) writeTar(ctx context.Context, tw *tar.Writer, fsys fs.FS, user
 			header.Devminor = int64(minor)
 		}
 		// work around some weirdness, without this we wind up with just the basename
-		header.Name = path
+		header.Name = filepath.Join(c.BaseDir, path)
 
 		// zero out timestamps for reproducibility
 		header.AccessTime = c.SourceDateEpoch
