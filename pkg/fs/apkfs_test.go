7a1fcf5f7b068c04b1271b2c2ca1b99a3cc63dbc
Violet White <violet.white.batt@gmail.com>
2023-10-13 14:24:54 -0700

    Improve the implementation of APKFS (#136)
    * Add `ReadDirFS` implementation
    * Implement tests for the control and package opening logic
    * Add test using WalkDir to test the ReadDirFS implemenation

--- pkg/fs/apkfs_test.go
+++ pkg/fs/apkfs_test.go
@@ -3,6 +3,7 @@ package fs
 import (
 	"context"
 	"io"
+	"io/fs"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,6 +21,18 @@ func TestReadAPKFile(t *testing.T) {
 		info, err := file.Stat()
 		require.Nil(t, err)
 		require.Equal(t, info.Name(), "hello")
+	})
+	t.Run("stat-control", func(t *testing.T) {
+		apkfs, err := NewAPKFS(context.Background(), "testdata/hello-2.12-r0.apk", APKFSControl)
+		require.Nil(t, err)
+		defer apkfs.Close()
+		require.NotNil(t, apkfs)
+		file, err := apkfs.Open("/.PKGINFO")
+		require.Nil(t, err)
+		defer file.Close()
+		info, err := file.Stat()
+		require.Nil(t, err)
+		require.Equal(t, info.Name(), ".PKGINFO")
 	})
 	t.Run("read", func(t *testing.T) {
 		apkfs, err := NewAPKFS(context.Background(), "testdata/hello-2.12-r0.apk", APKFSPackage)
@@ -45,5 +58,46 @@ func TestReadAPKFile(t *testing.T) {
 		}
 		require.Equal(t, info.Size(), readSoFar)
 		require.Equal(t, info.Name(), "hello")
+	})
+	t.Run("read-control", func(t *testing.T) {
+		apkfs, err := NewAPKFS(context.Background(), "testdata/hello-2.12-r0.apk", APKFSControl)
+		require.Nil(t, err)
+		defer apkfs.Close()
+		require.NotNil(t, apkfs)
+		file, err := apkfs.Open("/.PKGINFO")
+		require.Nil(t, err)
+		defer file.Close()
+		info, err := file.Stat()
+		require.Nil(t, err)
+		buffer := make([]byte, 4096)
+		var readSoFar int64
+		for {
+			readThisTime, err := file.Read(buffer)
+			if err != io.EOF {
+				require.Nil(t, err)
+			}
+			readSoFar += int64(readThisTime)
+			if readThisTime == 0 {
+				break
+			}
+		}
+		require.Equal(t, info.Size(), readSoFar)
+		require.Equal(t, info.Name(), ".PKGINFO")
+	})
+	t.Run("walk-fs", func(t *testing.T) {
+		apkfs, err := NewAPKFS(context.Background(), "testdata/hello-2.12-r0.apk", APKFSPackage)
+		require.Nil(t, err)
+		require.NotNil(t, apkfs)
+		defer apkfs.Close()
+		// Find a specific file by walking the filesystem
+		found := false
+		err = fs.WalkDir(apkfs, "/", func(name string, d fs.DirEntry, err error) error {
+			if d.Name() == "hello" {
+				found = true
+			}
+			return nil
+		})
+		require.Nil(t, err)
+		require.True(t, found)
 	})
 }
